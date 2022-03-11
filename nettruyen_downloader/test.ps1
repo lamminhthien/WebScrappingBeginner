907483928ac22eae79e9d53249d30c3b34338a84
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-03-11 08:47:46 +0700

    update code for nettruyendownloader

--- nettruyen_downloader/test.ps1
+++ nettruyen_downloader/test.ps1
@@ -1,44 +1,44 @@
-   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-1.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-1/548888
-                                                                                                                                              Start-Sleep 6
- chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-22.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-22/831422
-                                                                                                                                              Start-Sleep 6
- chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-21.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-21/823819
-                                                                                                                                              Start-Sleep 6
- chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-20.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-20/817200
-                                                                                                                                              Start-Sleep 6
- chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-19.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-19/815950
-                                                                                                                                              Start-Sleep 6
- chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-18.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-18/674409
-                                                                                                                                              Start-Sleep 6
- chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-17.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-17/673852
-                                                                                                                                              Start-Sleep 6
- chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-16.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-16/627815
-                                                                                                                                              Start-Sleep 6
- chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-15.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-15/627814
-                                                                                                                                              Start-Sleep 6
- chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-14.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-14/625271
-                                                                                                                                              Start-Sleep 6
- chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-13.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-13/625244
-                                                                                                                                              Start-Sleep 6
- chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-12.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-12/560962
-                                                                                                                                              Start-Sleep 6
- chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-11.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-11/558277
-                                                                                                                                              Start-Sleep 6
- chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-10.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-10/550014
-                                                                                                                                              Start-Sleep 6
-   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-9.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-9/549359
-                                                                                                                                              Start-Sleep 6
-   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-8.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-8/549089
-                                                                                                                                              Start-Sleep 6
-   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-7.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-7/548894
-                                                                                                                                              Start-Sleep 6
-   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-6.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-6/548893
-                                                                                                                                              Start-Sleep 6
-   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-5.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-5/548892
-                                                                                                                                              Start-Sleep 6
-   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-4.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-4/548891
-                                                                                                                                              Start-Sleep 6
-   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-3.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-3/548890
-                                                                                                                                              Start-Sleep 6
-   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\chap-2.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-2/548889
-                                                                                                                                              Start-Sleep 6
+   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-1.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-1/548888
+                                                                                                                                                                                                                Start-Sleep 6
+ chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-22.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-22/831422
+                                                                                                                                                                                                                Start-Sleep 6
+ chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-21.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-21/823819
+                                                                                                                                                                                                                Start-Sleep 6
+ chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-20.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-20/817200
+                                                                                                                                                                                                                Start-Sleep 6
+ chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-19.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-19/815950
+                                                                                                                                                                                                                Start-Sleep 6
+ chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-18.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-18/674409
+                                                                                                                                                                                                                Start-Sleep 6
+ chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-17.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-17/673852
+                                                                                                                                                                                                                Start-Sleep 6
+ chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-16.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-16/627815
+                                                                                                                                                                                                                Start-Sleep 6
+ chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-15.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-15/627814
+                                                                                                                                                                                                                Start-Sleep 6
+ chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-14.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-14/625271
+                                                                                                                                                                                                                Start-Sleep 6
+ chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-13.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-13/625244
+                                                                                                                                                                                                                Start-Sleep 6
+ chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-12.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-12/560962
+                                                                                                                                                                                                                Start-Sleep 6
+ chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-11.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-11/558277
+                                                                                                                                                                                                                Start-Sleep 6
+ chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-10.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-10/550014
+                                                                                                                                                                                                                Start-Sleep 6
+   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-9.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-9/549359
+                                                                                                                                                                                                                Start-Sleep 6
+   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-8.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-8/549089
+                                                                                                                                                                                                                Start-Sleep 6
+   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-7.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-7/548894
+                                                                                                                                                                                                                Start-Sleep 6
+   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-6.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-6/548893
+                                                                                                                                                                                                                Start-Sleep 6
+   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-5.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-5/548892
+                                                                                                                                                                                                                Start-Sleep 6
+   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-4.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-4/548891
+                                                                                                                                                                                                                Start-Sleep 6
+   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-3.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-3/548890
+                                                                                                                                                                                                                Start-Sleep 6
+   chrome --enable-logging --headless --disable-gpu --print-to-pdf=d:\Code\pythonProjectGroup\WebScrappingBeginner\nettruyen_downloader\chap-2.pdf http://www.nettruyenmoi.com/truyen-tranh/protagonist-x-rival/chap-2/548889
+                                                                                                                                                                                                                Start-Sleep 6
